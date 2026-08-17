3ca234c601fe002095b04f31a922767c7cdcad5a
Yiwen Wang <wangyiwenau@gmail.com>
2026-08-17 11:03:29 +1000

    update

--- src/pages/HolyGrailPage/HolyGrailPage.tsx
+++ src/pages/HolyGrailPage/HolyGrailPage.tsx
@@ -2,13 +2,13 @@ import type { FC } from "react";
 
 const HolyGrailPage: FC = () => (
   <div className="fixed inset-0 flex flex-col text-center">
-    <header className="h-15 bg-orange-500">Header</header>
+    <header className="h-12 bg-orange-600">Header</header>
     <div className="flex grow">
-      <nav className="w-25 bg-orange-400">Navigation</nav>
+      <nav className="w-25 bg-orange-500">Navigation</nav>
       <main className="grow bg-orange-200">Main</main>
-      <aside className="w-25 bg-orange-300">Sidebar</aside>
+      <aside className="w-25 bg-orange-400">SideBar</aside>
     </div>
-    <footer className="h-25 bg-gray-500">Footer</footer>
+    <footer className="h-25 bg-gray-700">Footer</footer>
   </div>
 );
 
