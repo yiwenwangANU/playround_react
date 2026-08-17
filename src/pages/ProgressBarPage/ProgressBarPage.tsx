0c65f91f7d67de8876dd794b40ba8a5cad86f15c
Yiwen Wang <wangyiwenau@gmail.com>
2026-08-17 11:25:46 +1000

    update

--- src/pages/ProgressBarPage/ProgressBarPage.tsx
+++ src/pages/ProgressBarPage/ProgressBarPage.tsx
@@ -3,17 +3,13 @@ import ProgressBar from "./components/ProgressBar";
 
 const ProgressBarPage: FC = () => {
   const [num, setNum] = useState<number>(0);
+
   return (
     <>
       {Array.from({ length: num }, () => (
         <ProgressBar />
       ))}
-      <button
-        className="py-1/2 cursor-pointer rounded border border-gray-400 bg-gray-200 px-2 text-sm"
-        onClick={() => setNum((prev) => prev + 1)}
-      >
-        Add
-      </button>
+      <button onClick={() => setNum((prev) => prev + 1)}>Add</button>
     </>
   );
 };
--- src/pages/ProgressBarPage/components/ProgressBar/ProgressBar.tsx
+++ src/pages/ProgressBarPage/components/ProgressBar/ProgressBar.tsx
@@ -1,18 +1,23 @@
 import { useEffect, useState, type FC } from "react";
-import clsx from "clsx";
+import { clsx } from "clsx";
 
-const ProgressBar: FC = () => {
+interface Props {
+  delay?: number;
+}
+
+const ProgressBar: FC<Props> = ({ delay = 2000 }) => {
   const [active, setActive] = useState<boolean>(false);
   useEffect(() => setActive(true), []);
 
   return (
-    <div className="my-2 h-2 w-full bg-gray-200">
+    <div className="h-2 w-full bg-gray-200 my-1">
       <div
         className={clsx(
-          "h-2 bg-green-800 transition-[width] duration-2000",
-          { "w-full": active },
+          "h-2 bg-green-700",
           { "w-0": !active },
+          { "w-full": active },
         )}
+        style={{ transitionDuration: `${delay}ms` }}
       />
     </div>
   );
