9b8d48ee8fd13e597dcf3cc8fb974275fb186ed4
Yiwen Wang <wangyiwenau@gmail.com>
2026-08-18 11:32:08 +1000

    delete tempeture

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
 import { Routes, Route } from "react-router";
-import TemperatureConverterPage from "./pages/TemperatureConverterPage";
 
 const App = () => (
   <Routes>
-    <Route index element={<TemperatureConverterPage />} />
+
   </Routes>
 );
 
