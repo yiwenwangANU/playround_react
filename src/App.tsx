ba7b63b4f4f152a210f771aa7e91e0459ef8f400
Yiwen Wang <wangyiwenau@gmail.com>
2026-08-19 14:04:56 +1000

    restore main

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
 import { Routes, Route } from "react-router";
-import StarRatingPage from "./pages/StarRatingPage";
 
 const App = () => (
   <Routes>
-    <Route index element={<StarRatingPage />} />
   </Routes>
 );
 
