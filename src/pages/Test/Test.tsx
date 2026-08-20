8cff6efebd9f28b5af268491083a3df1dddca38a
Yiwen Wang <wangyiwenau@gmail.com>
2026-08-21 08:13:54 +1000

    03

--- src/pages/Test/Test.tsx
+++ src/pages/Test/Test.tsx
@@ -1,62 +1,15 @@
 import type { FC } from "react";
-import { useForm } from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { z } from "zod";
-import useCreateContact from "./hooks/useCreateContact";
 
-const schema = z.object({
-  name: z.string(),
-  email: z.email("lol"),
-  message: z.string(),
-});
-
-export type Schema = z.infer<typeof schema>;
-
-const Test: FC = () => {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm({
-    resolver: zodResolver(schema),
-  });
-  const { data, trigger, isMutating } = useCreateContact();
-  return (
-    <form
-      className="mx-auto flex w-fit flex-col gap-2"
-      onSubmit={handleSubmit((data) => {
-        trigger(data);
-      })}
-    >
-      <label>Name</label>
-      <input
-        {...register("name")}
-        className="border border-black px-1 py-0.5"
-      />
-      <label>Email</label>
-      <input
-        {...register("email")}
-        className="border border-black px-1 py-0.5"
-      />
-      {errors.email && (
-        <span className="text-red-500">{errors.email.message}</span>
-      )}
-      <label>Message</label>
-      <textarea
-        {...register("message")}
-        className="border border-black px-1 py-0.5"
-      />
-      <button
-        type="submit"
-        disabled={isMutating}
-        className="w-fit cursor-pointer rounded border border-gray-400 bg-gray-200 px-1.5 py-0.5"
-      >
-        Submit
-      </button>
-      {isMutating && <span>Sending form...</span>}
-      {data && <span className="text-green-500">{data}</span>}
-    </form>
-  );
-};
+const Test: FC = () => (
+  <div className="fixed inset-0 flex flex-col text-center">
+    <header className="h-12 bg-orange-700">Header</header>
+    <div className="flex grow">
+      <nav className="w-20 bg-orange-500">Navigation</nav>
+      <main className="grow bg-orange-200">Main</main>
+      <aside className="w-20 bg-amber-400">Sidebar</aside>
+    </div>
+    <footer className="h-20 bg-gray-600">Footer</footer>
+  </div>
+);
 
 export default Test;
