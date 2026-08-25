ec0c97f943cbcbfee9c5d7ae6674e6c31f3c1538
Yiwen Wang <wangyiwenau@gmail.com>
2026-08-25 12:10:52 +1000

    new selected cells

--- src/pages/SelectableCellsPage/SelectableCellsPage.tsx
+++ src/pages/SelectableCellsPage/SelectableCellsPage.tsx
@@ -1,4 +1,5 @@
-import { useReducer, type FC } from "react";
+import { useEffect, useReducer, useRef, useState, type FC } from "react";
+import clsx from "clsx";
 
 const GRID_ROWS = 12;
 const GRID_COLS = 12;
@@ -32,19 +33,56 @@ const reducer = (drag: Drag, action: Action) => {
   }
 };
 
+const getSelectedCells = (
+  start: Point,
+  current: Point,
+  cells: (HTMLDivElement | null)[],
+): number[] => {
+  const left = Math.min(start.x, current.x);
+  const right = Math.max(start.x, current.x);
+  const top = Math.min(start.y, current.y);
+  const bottom = Math.max(start.y, current.y);
+
+  const selected = cells.reduce<number[]>((selectedIndex, cell, i) => {
+    if (!cell) return [];
+    const rect = cell.getBoundingClientRect();
+    if (
+      bottom >= rect.top &&
+      top <= rect.bottom &&
+      right >= rect.left &&
+      left <= rect.right
+    )
+      return [...selectedIndex, i];
+    return selectedIndex;
+  }, []);
+
+  return selected;
+};
+
 const SelectableCellsPage: FC = () => {
+  const [selectedCells, setSelectedCells] = useState<number[]>([]);
+  const cellsRef = useRef<(HTMLDivElement | null)[]>([]);
   const [drag, dispatch] = useReducer(reducer, {
     start: null,
     current: null,
     isDragging: false,
   });
 
+  useEffect(() => {
+    if (!drag.start || !drag.current || !drag.isDragging) return;
+    const cells = getSelectedCells(drag.start, drag.current, cellsRef.current);
+    setSelectedCells(cells);
+  }, [drag.current, drag.isDragging, drag.start]);
   return (
     <div
       className="fixed inset-0"
-      onPointerDown={(e) =>
-        dispatch({ type: "pointerdown", point: { x: e.clientX, y: e.clientY } })
-      }
+      onPointerDown={(e) => {
+        setSelectedCells([]);
+        dispatch({
+          type: "pointerdown",
+          point: { x: e.clientX, y: e.clientY },
+        });
+      }}
       onPointerMove={(e) =>
         dispatch({ type: "pointermove", point: { x: e.clientX, y: e.clientY } })
       }
@@ -68,8 +106,16 @@ const SelectableCellsPage: FC = () => {
           className="grid border-t border-l"
           style={{ gridTemplateColumns: `repeat(${GRID_COLS}, 1fr)` }}
         >
-          {Array.from({ length: GRID_COLS * GRID_ROWS }, () => (
-            <div className="h-10 w-10 border-r border-b" />
+          {Array.from({ length: GRID_COLS * GRID_ROWS }, (_, i) => (
+            <div
+              className={clsx("h-10 w-10 border-r border-b", {
+                "bg-sky-100": selectedCells.includes(i),
+              })}
+              key={i}
+              ref={(cell) => {
+                cellsRef.current[i] = cell;
+              }}
+            />
           ))}
         </div>
       </div>
