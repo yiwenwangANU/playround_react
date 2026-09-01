7f886baca3ddb420ea842a47e0a9f8bc9d4db469
Yiwen Wang <wangyiwenau@gmail.com>
2026-09-02 08:18:47 +1000

    add error boundary and update eslint

--- eslint.config.js
+++ eslint.config.js
@@ -2,26 +2,28 @@ import js from "@eslint/js";
 import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
-import jsxA11y from "eslint-plugin-jsx-a11y";
 import tseslint from "typescript-eslint";
 import { defineConfig, globalIgnores } from "eslint/config";
 
 export default defineConfig([
   globalIgnores(["dist"]),
+
   {
     files: ["**/*.{ts,tsx}"],
+
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
-      jsxA11y.flatConfigs.recommended,
     ],
+
     languageOptions: {
       globals: globals.browser,
     },
+
     rules: {
-      "react-hooks/exhaustive-deps": "error",
+      "react-hooks/exhaustive-deps": "warn",
     },
   },
 ]);
