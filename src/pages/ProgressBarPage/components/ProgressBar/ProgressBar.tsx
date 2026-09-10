9673875e773bd8a73bd83a836238d6bf164dbcd2
Yiwen Wang <wangyiwenau@gmail.com>
2026-09-11 09:12:31 +1000

    add aria role

--- src/pages/ProgressBarPage/components/ProgressBar/ProgressBar.tsx
+++ src/pages/ProgressBarPage/components/ProgressBar/ProgressBar.tsx
@@ -6,8 +6,12 @@ interface Props {
 
 const ProgressBar: FC<Props> = ({ progress }) => {
   const refinedProgress = Math.max(Math.min(progress, 100), 0);
+
   return (
-    <div className="mx-10 h-4 overflow-hidden rounded-3xl border border-gray-400 bg-gray-200">
+    <div
+      className="mx-10 h-4 overflow-hidden rounded-3xl border border-gray-400 bg-gray-200"
+      role="progressbar"
+    >
       <div
         className="flex h-4 items-center justify-center overflow-clip bg-blue-500 text-sm text-white"
         style={{ width: `${refinedProgress}%` }}
