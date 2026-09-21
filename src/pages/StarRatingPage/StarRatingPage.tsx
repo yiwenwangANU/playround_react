3b3ad9c594a0d587d91e668c28c12da74b8845b0
Yiwen Wang <wangyiwenau@gmail.com>
2026-09-21 16:47:19 +1000

    re-write

--- src/pages/StarRatingPage/StarRatingPage.tsx
+++ src/pages/StarRatingPage/StarRatingPage.tsx
@@ -1,17 +1,34 @@
 import { useState, type FC } from "react";
-import StarRating from "./components/StarRating/StarRating";
+import { Star } from "lucide-react";
+import clsx from "clsx";
 
-const INITIAL_RATE = 3;
+const MAX_STARS = 5;
+const INITIAL_STAR = 3;
+
 const StarRatingPage: FC = () => {
-  const [rate, setRate] = useState<number>(INITIAL_RATE);
-  const handleClickStar = (star: number) => {
-    setRate(star);
-  };
+  const [star, setStar] = useState<number>(INITIAL_STAR);
+  const [hoverStar, setHoverStar] = useState<number>(0);
+
   return (
-    <>
-      <StarRating startRate={rate} maxStar={5} onClickStar={handleClickStar} />
-      <div>Star Rate {rate}</div>
-    </>
+    <main>
+      <div className="flex">
+        {Array.from({ length: MAX_STARS }, (_, i) => (
+          <button
+            onClick={() => setStar(i + 1)}
+            onPointerEnter={() => setHoverStar(i + 1)}
+            onPointerLeave={() => setHoverStar(0)}
+          >
+            <Star
+              className={clsx({
+                "text-amber-500":
+                  (hoverStar && i < hoverStar) || (!hoverStar && i < star),
+              })}
+            />
+          </button>
+        ))}
+      </div>{" "}
+      <div>Current Star: {star}</div>
+    </main>
   );
 };
 
